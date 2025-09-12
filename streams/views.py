afe3125bb0d6e2394e174814dfdbf1e67dd67267
StahlSol <imperialistix@gmail.com>
2025-09-11 22:36:41 -0600

    Add YouTube API call to find first upcoming livestream

--- streams/views.py
+++ streams/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import os
+from googleapiclient.discovery import build
 
-# Create your views here.
+def upcoming(request):
+    api_key = os.getenv('YOUTUBE_API_KEY')
+
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    request_api = youtube.search().list(
+        part='snippet',
+        channelId='UChAnqc_AY5_I3Px5dig3X1Q',
+        eventType='upcoming',
+        type='video',
+        maxResults=1,
+    )
+    response=request_api.execute()
+
+    items = response.get('items', [])
+    streams = []
+    for item in items:
+        title = item['snippet']['title']
+        video_id = item['id']['videoId']
+        url = f'https://www.youtube.com/watch?v={video_id}'
+        streams.append({'title':title, 'url':url})
+
+    return render(request, 'streams/upcoming.html', {'streams': streams})
